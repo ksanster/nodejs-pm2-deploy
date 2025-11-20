210548dfd199b80b90a79798db5c90a376daf2b4
a.semin <a.semin@corp.mail.ru>
2025-11-20 22:18:58 +0300

    - Add ecosystem config - Add crash-test endpoint - Update npm start on frontend

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Деплой приложения на сервер с использованием pm2
+- IP адрес 158.160.76.243
+- Frontend https://ksanster.students.nomorepartiessbs.ru
+- Backend https://api.ksanster.students.nomorepartiessbs.ru
 
-Стартеркит проекта по автоматизации деплоя фронтенда и бэкенда при помощи pm2 (pm2 deploy)
---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_REPO, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'nodejs-mesto-project',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
